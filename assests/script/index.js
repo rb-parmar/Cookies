def9cf2ba96f951710395b59683cc64c5cc24b59
Rushit <rushitparmar59@gmail.com>
2023-03-31 11:43:43 -0500

    Added JS code

--- assests/script/index.js
+++ assests/script/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 // Declaring HTML elements
+const text = document.querySelector('.text');
+
 const dialog1 = document.querySelector('.dialog1');
 const acceptButton = document.querySelector('.accept');
 const settingButton = document.querySelector('.settings');
+
 const dialog2 = document.querySelector('.dialog2');
 const browser = document.querySelector('.browser input');
 const os = document.querySelector('.os input');
@@ -11,120 +14,142 @@ const width = document.querySelector('.width input');
 const height = document.querySelector('.height input');
 const saveButton = document.querySelector('.save');
 
+
+console.log(document.cookie ? 'Cookies available' : 'No cookies found');
+
+
+// Adding the delay for dialog1
+setTimeout(() => {
+  dialog1.style.display = 'block';
+  text.classList.add('opacity');
+}, 900);
+
 // function to return width
 function getWidth() {
   const width = window.innerWidth;
-  console.log(`Width: ${width} px`);
+  return width;
 }
 
 // function to return Height
 function getHeight() {
   const height = window.innerHeight;
-  console.log(`Height: ${height} px`);
-}
+  return height;
+};
 
 // function to return browser 
 // got the function from google
 function getBrowser() {
-  let userAgent = navigator.userAgent;
-  let browserName;
   
-  if(userAgent.match(/chrome|chromium|crios/i)){
-      browserName = "chrome";
-    }else if(userAgent.match(/firefox|fxios/i)){
-      browserName = "firefox";
-    }  else if(userAgent.match(/safari/i)){
-      browserName = "safari";
-    }else if(userAgent.match(/opr\//i)){
-      browserName = "opera";
-    } else if(userAgent.match(/edg/i)){
-      browserName = "edge";
-    }else{
-      browserName="No browser detection";
+  if(navigator.userAgent.match(/chrome|chromium|crios/i)){
+       return "chrome";
+    } else if(navigator.userAgent.match(/firefox|fxios/i)){
+       return "firefox";
+    } else if(navigator.userAgent.match(/safari/i)){
+       return "safari";
+    } else if(navigator.userAgent.match(/opr\//i)){
+       return "opera";
+    } else if(navigator.userAgent.match(/edg/i)){
+       return "edge";
+    } else{
+       return "rejected";
     }
-
-  console.log(`Browser: ${browserName}`);
 }
 
 // function to return the os
 // got the function from google
 function getOS() {
-  var userAgent = window.navigator.userAgent,
-      platform = window.navigator.userAgentData.platform,
-      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
-      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-      os = null;
+  var platform = navigator.userAgentData.platform;
+  var macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+  var windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+  var iosPlatforms = ['iPhone', 'iPad', 'iPod'];
 
   if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'Mac OS';
+    return 'Mac OS';
   } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = 'iOS';
+    return 'iOS';
   } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'Windows';
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android';
-  } else if (/Linux/.test(platform)) {
-    os = 'Linux';
-  }
+    return 'Windows';
+  } 
 
-  return os;
+  return 'rejected';
 }
 
 
-function setCookie(name, value, options = {}) {
-  options = {
-    path: '/',
-    SameSite: 'Lax',
-    ...options
-  };
+// function to set a cookies
+function setCookie(name, value) {
 
-  const keys = Object.keys(options);
-  const values = Object.values(options);
-
-  if (options.expires instanceof Date) {
-    options.expires = options.expires.toUTCString();
-  }
-
-  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-
-  for (let i = 0; i < keys.length; i++) {
-    updatedCookie += `; ${keys[i]}=${values[i]}`;
-  }
+  const date = new Date();
+  date.setSeconds(date.getSeconds(), 15000);
+  let expiryTime = date.toUTCString();
+  let updatedCookie = `${name}=${value}; path=/; expires=${expiryTime}`;
 
   document.cookie = updatedCookie;
 }
 
-/*
-  ?:  -> Non-capturing group (the regex inside the parenthesis must be matched
-         but does not create the capturing group)
-  ^|; -> Not the following
-*/
-
+// function to get cookie
 function getCookie(name) {
   let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
   ));
 
-  return matches ? decodeURIComponent(matches[1]) : undefined;
+  return matches ? consloe.log(decodeURIComponent(matches[1])) : undefined;
 }
 
-function deleteCookie(name) {
-  setCookie(name, '', {'max-age': -1});
+
+// function to create cookies based on user preferences
+function createCookie() {
+  // checking browser
+  if (browser.checked) {
+    setCookie('Browser', `${getBrowser()}`);
+  } else {
+    setCookie('Browser', 'rejected');
+  }
+
+  // checking os
+  if (os.checked) {
+    setCookie('Operating system', `${getOS()}`);
+  } else {
+    setCookie('Operating system', 'rejected');
+  }
+
+  // checking width
+  if (width.checked) {
+    setCookie('Width', `${getWidth()}`);
+  } else {
+    setCookie('Width', 'rejected');
+  }
+
+  // checking height
+  if (height.checked) {
+    setCookie('Height', `${getHeight}`);
+  } else {
+    setCookie('Height', 'rejected');
+  }
 }
 
-// document.cookie = `name=${encodeURIComponent('Andre Specht')}; path=/; max-age=15; SameSite=Lax`;
-// document.cookie = `city=${encodeURIComponent('Winnipeg')}; path=/; max-age=15; SameSite=Lax`;
+// Adding event listener to buttons
+acceptButton.addEventListener('click', function() {
+  dialog1.style.display = 'none';
+  createCookie();
+  getCookie('Browser');
+  getCookie('Operating system');
+  getCookie('Width');
+  getCookie('Height');
+  text.classList.remove('opacity');
+});
 
-const date = new Date();
-date.setSeconds(date.getSeconds() + 15);
+settingButton.addEventListener('click', function() {
+  dialog1.style.display = 'none';
+  dialog2.style.display = 'block';
+});
 
-setCookie('name', 'Andre Specht', {'max-age': 15});
-setCookie('city', 'New York', {'expires': date});
-
-console.log(getCookie('name'));
-console.log(getCookie('city'));
-console.log(getCookie('email'));
-
-// deleteCookie('name');
+saveButton.addEventListener('click', function() {
+  dialog2.style.display = 'none';
+  createCookie();
+  getCookie('Browser');
+  getCookie('Operating system');
+  getCookie('Width');
+  getCookie('Height');
+  text.classList.remove('opacity');
+});
 
